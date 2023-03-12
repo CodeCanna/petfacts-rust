ab5bc7b06e4f49aad176b0fee90271950fdf8e01
CodeCanna <codecannamw@gmail.com>
2023-03-12 11:54:21 -0600

    Improved error messages.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-use std::{env, io::stdout, time::Duration, thread, io::Write};
-use reqwest::{self, Result};
-use rand::{thread_rng, Rng};
 use json;
+use rand::{thread_rng, Rng};
+use reqwest::{self, Result};
+use std::{env, io::stdout, io::Write, thread, time::Duration};
+
+fn print_error_message(animal: &str) -> String {
+    let cat_error_message = String::from("Failed to retrieve Cat Fact, check internet connection.");
+    let dog_error_message = String::from("Failed to retrieve Dog Fact, check internet connection.");
+
+    if animal == "cat" {
+        cat_error_message
+    } else {
+        dog_error_message
+    }
+}
 
 // Get the fact data based on the URL that is passed
 fn get_fact(url: &str) -> Result<String> {
-    reqwest::blocking::get(url)
-        .expect("Couldn't get fact, please check internet connection.")
-        .text()
+    match reqwest::blocking::get(url) {
+        Ok(fact) => return fact.text(),
+        Err(e) => return Err(e),
+    };
 }
 
 // Return dog or cat as a string randomly
@@ -24,9 +36,9 @@ fn type_string(string_to_type: &String) {
     for character in string_to_type.chars() {
         print!("{character}");
         stdout().flush().expect("Something is wrong with stdout!");
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(50));
     }
-    println!("");
+    println!(""); // Go to the next line when done "typing"
 }
 
 // Print out a help screen
@@ -42,29 +54,59 @@ fn print_help() {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 { // If an argument is passed handle it
+    if args.len() > 1 {
+        // If an argument is passed handle it
         match args[1].to_lowercase() {
-            _ if "cat".to_string() == args[1] => type_string(&json::parse(&get_fact("https://catfact.ninja/fact").unwrap())
-                                                    .unwrap()["fact"]
-                                                    .to_string()),
-            _ if "dog".to_string() == args[1] => type_string(&json::parse(&get_fact("http://dog-api.kinduff.com/api/facts?number=1").unwrap())
-                                                    .unwrap()["facts"][0]
-                                                    .to_string()),
+            _ if "cat".to_string() == args[1] => {
+                let fact = get_fact("https://catfact.ninja/fact");
+                match fact {
+                    Ok(f) => {
+                        let fact_json = json::parse(&f.as_str()).unwrap();
+                        type_string(&fact_json["fact"].to_string());
+                    }
+
+                    Err(_) => println!("{}", print_error_message("cat")),
+                };
+            }
+            _ if "dog".to_string() == args[1] => {
+                let fact = get_fact("http://dog-api.kinduff.com/api/facts?number=1");
+                match fact {
+                    Ok(f) => {
+                        let fact_json = json::parse(&f.to_string()).unwrap();
+                        type_string(&fact_json["facts"][0].to_string());
+                    }
+
+                    Err(_) => println!("{}", print_error_message("dog")),
+                };
+            }
             _ if "help".to_string() == args[1] => print_help(),
             _ => {
                 println!("Invalid option: {}", args[1]);
                 print_help();
             }
         };
-    } else { // If no argument is passed choose a dog or a cat fact randomly
+    } else {
+        // If no argument is passed choose a dog or a cat fact randomly
         if random_animal() == "cat".to_string() {
-            type_string(&json::parse(&get_fact("https://catfact.ninja/fact").unwrap())
-                .unwrap()["fact"]
-                .to_string());
+            let fact = get_fact("https://catfact.ninja/fact");
+            match fact {
+                Ok(f) => {
+                    let fact_json = json::parse(&f.as_str()).unwrap();
+                    type_string(&fact_json["fact"].to_string());
+                }
+
+                Err(_) => println!("{}", print_error_message("cat")),
+            };
         } else {
-            type_string(&json::parse(&get_fact("http://dog-api.kinduff.com/api/facts?number=1").unwrap())
-                .unwrap()["facts"][0]
-                .to_string());
+            let fact = get_fact("http://dog-api.kinduff.com/api/facts?number=1");
+            match fact {
+                Ok(f) => {
+                    let fact_json = json::parse(&f.to_string()).unwrap();
+                    type_string(&fact_json["facts"][0].to_string());
+                }
+
+                Err(_) => println!("{}", print_error_message("dog")),
+            };
         }
     }
 }
